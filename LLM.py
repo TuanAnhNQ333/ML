1bc3a4a82078d6a02f1133a1881bd4c03ad1386e
Tuan Anh <tuananh030305@gmail.com>
2024-10-24 00:12:52 +0700

    main

--- LLM.py
+++ LLM.py
@@ -9,3 +9,15 @@ to fruitfully process large natural language corpora."""
 
 print(sent_tokenize(text))
 print(word_tokenize(text))
+
+import spacy
+
+# Creating blank language object then tokenizing words of the sentence
+nlp = spacy.blank("en")
+
+doc = nlp("""Natural language processing (NLP) is a field of computer science, artificial intelligence and computational linguistics
+concerned with the interactions between computers and human languages, and, in particular, concerned with programming computers
+to fruitfully process large natural language corpora.""")
+
+for token in doc:
+    print(token)
