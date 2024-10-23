51bb32ce63c1888402f645e857008a3f4f784623
Tuan Anh <tuananh030305@gmail.com>
2024-10-24 00:11:38 +0700

    main

--- LLM.py
+++ LLM.py
@@ -1,2 +1,11 @@
 import nltk
 nltk.download('punkt')
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+text = """Natural language processing (NLP) is a field of computer science, artificial intelligence and computational linguistics
+concerned with the interactions between computers and human languages, and, in particular, concerned with programming computers
+to fruitfully process large natural language corpora."""
+
+print(sent_tokenize(text))
+print(word_tokenize(text))
